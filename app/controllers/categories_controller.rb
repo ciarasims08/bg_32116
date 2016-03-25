052afd53ca98b87f43a2905058eaf95f819faeed
Ciara Sims <ciarasims08@gmail.com>
2016-03-25 10:49:19 -0400

    Added crud ability to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,20 +19,20 @@ class CategoriesController < ApplicationController
 	def create
 		@category = Category.new(category_params)
 		
-		# if @category.save
-		# 	redirect_to @category
-		# else
-		# 	render 'new'
-		# end
+		if @category.save
+			redirect_to @category
+		else
+			render 'new'
+		end
 	end
 
 	def update
 		@category = Category.find(params[:id])
-		# if @category.update(category_params)
-		# 	redirect_to @category
-		# else
-		# 	render 'edit'
-		# end
+		if @category.update(category_params)
+			redirect_to @category
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
